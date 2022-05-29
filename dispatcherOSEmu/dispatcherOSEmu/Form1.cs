6a878a7c84b61a8d86ac47a427ff7ec003280be9
egor <egorka.leps@mail.ru>
2022-05-30 04:48:38 +0700

    code formatting completed

--- dispatcherOSEmu/dispatcherOSEmu/Form1.cs
+++ dispatcherOSEmu/dispatcherOSEmu/Form1.cs
@@ -1,188 +1,160 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
 
 namespace dispatcherOSEmu
 {
 
-   public partial class Form1 : Form
-   {
-      Form2 f2 = new Form2();
-      //Timer t1 = new Timer();
-      private Dictionary<string, int> tasks = new Dictionary<string, int>();
-      Queue<string> currentTasks = new Queue<string>();
-      bool go = false;
-        int maxQuantTime = 100;
+    public partial class Form1 : Form
+    {
+        Form2 f2 = new Form2();
+        private Dictionary<string, int> tasks = new Dictionary<string, int>();
+        Queue<string> currentTasks = new Queue<string>();
+        bool go = false;
         int minQuantTime = 1;
 
-      float T;
+        float T;
 
-      public Form1()
-      {
-         InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
+        /**
+         *  Начать обработку рчереди
+         *  button2 = "Начать выполнение"
+         */
+        private async void button2Click(object sender, EventArgs e)
+        {
+            int quantTime = Int32.Parse(textBox3.Text);
 
-      }
-
-      /**
-       *  Начать обработку рчереди
-       *  button2 = "Начать выполнение"
-       */
-      private async void button2Click(object sender, EventArgs e)
-      {
-           int quantTime = Int32.Parse(textBox3.Text);
-
-            if(quantTime < minQuantTime)
+            if (quantTime < minQuantTime)
             {
                 MessageBox.Show("Quant time is less than minumum allowed");
                 return;
             }
 
-            if (quantTime > minQuantTime)
-            {
-                MessageBox.Show("Quant time is more than maximum allowed");
-                return;
-            }
-
-
             T = currentTasks.Count * quantTime;
 
-         go = true;
-         if (!checkBox1.Checked)
-         {
-            while (currentTasks.Count != 0)
+            go = true;
+            if (!checkBox1.Checked)
             {
-               int kwant = (int)(T / currentTasks.Count);
-               textBox3.Text = kwant.ToString();
-               progressBar1.Value = 0;
-               var key = currentTasks.Dequeue();
-               if (tasks[key] <= kwant) kwant = tasks[key];
+                while (currentTasks.Count != 0)
+                {
+                    int kwant = (int)(T / currentTasks.Count);
+                    textBox3.Text = kwant.ToString();
+                    progressBar1.Value = 0;
+                    var key = currentTasks.Dequeue();
+                    if (tasks[key] <= kwant) kwant = tasks[key];
 
-               for (int i = 0; i < 10; i++)
-               {
-                  await Task.Delay(kwant * 100);
-                  progressBar1.PerformStep();
-               }
-
-
-               tasks[key] -= kwant;
-
-               if (tasks[key] > 0) currentTasks.Enqueue(key);
-               else
-               {
-                  tasks.Remove(key);
-                  textBox2.AppendText(key);
-               }
-
-               addTasksText(this.tasks);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        await Task.Delay(kwant * 100);
+                        progressBar1.PerformStep();
+                    }
 
 
-            }
-            go = false;
-         }
+                    tasks[key] -= kwant;
 
-      }
+                    if (tasks[key] > 0) currentTasks.Enqueue(key);
+                    else
+                    {
+                        tasks.Remove(key);
+                        textBox2.Text += key + Environment.NewLine;
+                    }
 
-
-      private void Form1_Load(object sender, EventArgs e)
-      {
-
-      }
-
-      private void label5_Click(object sender, EventArgs e)
-      {
-
-      }
-
-      private void label6_Click(object sender, EventArgs e)
-      {
-
-      }
-
-      private void panel1_Paint(object sender, PaintEventArgs e)
-      {
-
-      }
-
-
-      private void addTasksText(Dictionary<string, int> tasks)
-      {
-         textBox1.Text = "";
-
-         foreach (var task in tasks)
-         {
-            textBox1.AppendText(String.Format("{0,-20}{1,-20}", task.Key, task.Value));
-            textBox1.AppendText(Environment.NewLine);
-
-         }
-      }
-
-      /**
-       * Генерацмя списка 
-       * button1 = сгенерировать список
-       */
-      private void button1_Click(object sender, EventArgs e)
-      {
-         f2.ShowDialog();
-         this.tasks = f2.getTasks();
-         currentTasks = new Queue<string>(tasks.Keys);
-         addTasksText(this.tasks);
-
-      }
-
-      private void progressBar1_Click(object sender, EventArgs e)
-      {
-
-      }
-
-      private void Form1_KeyDown(object sender, KeyEventArgs e)
-      {
-
-         
-         
-      }
-
-      private void button4_Click(object sender, EventArgs e)
-      {
-         if (go)
-            step();
-      }
-      async private void step()
-      {
-         if(currentTasks.Count != 0)
-         {
-            int kwant = (int)(T / currentTasks.Count);
-            textBox3.Text = kwant.ToString();
-            progressBar1.Value = 0;
-            var key = currentTasks.Dequeue();
-            if (tasks[key] <= kwant) kwant = tasks[key];
-
-            for (int i = 0; i < 10; i++)
-            {
-               await Task.Delay(kwant * 10);
-               progressBar1.PerformStep();
-            }
-            tasks[key] -= kwant;
-
-            if (tasks[key] > 0) currentTasks.Enqueue(key);
-            else
-            {
-               tasks.Remove(key);
-               textBox2.AppendText(key);
+                    addTasksText(this.tasks);
+                }
+                go = false;
             }
 
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e){}
+
+        private void label5_Click(object sender, EventArgs e){}
+
+        private void label6_Click(object sender, EventArgs e){}
+
+        private void panel1_Paint(object sender, PaintEventArgs e){}
+
+
+        private void addTasksText(Dictionary<string, int> tasks)
+        {
+            textBox1.Text = "";
+
+            foreach (var task in tasks)
+            {
+                textBox1.AppendText(String.Format("{0,-20}{1,-20}", task.Key, task.Value));
+                textBox1.AppendText(Environment.NewLine);
+
+            }
+        }
+
+        /**
+         * Генерацмя списка 
+         * button1 = сгенерировать список
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f2.ShowDialog();
+            this.tasks = f2.getTasks();
+            currentTasks = new Queue<string>(tasks.Keys);
             addTasksText(this.tasks);
 
-            if (currentTasks.Count == 0) go = false;
+        }
 
-         }
-      }
-   }
+        private void progressBar1_Click(object sender, EventArgs e){}
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) { }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (go)
+                step();
+        }
+        async private void step()
+        {
+            if (currentTasks.Count != 0)
+            {
+                int kwant = (int)(T / currentTasks.Count);
+                textBox3.Text = kwant.ToString();
+                progressBar1.Value = 0;
+                var key = currentTasks.Dequeue();
+                if (tasks[key] <= kwant) kwant = tasks[key];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(kwant * 10);
+                    progressBar1.PerformStep();
+                }
+                tasks[key] -= kwant;
+
+                if (tasks[key] > 0) currentTasks.Enqueue(key);
+                else
+                {
+                    tasks.Remove(key);
+                    textBox2.AppendText(key);
+                    textBox2.AppendText(Environment.NewLine);
+                }
+
+                addTasksText(this.tasks);
+
+                if (currentTasks.Count == 0) go = false;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tasks.Clear();
+            currentTasks.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "10";
+            progressBar1.Value = 0;
+        }
+    }
 }
--- dispatcherOSEmu/dispatcherOSEmu/Form2.cs
+++ dispatcherOSEmu/dispatcherOSEmu/Form2.cs
@@ -1,37 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace dispatcherOSEmu
 {
-   public partial class Form2 : Form
-   {
-      private const int MAX_TASK_NAME_LENGTH = 10;
-      private const int MAX_TASK_TIME_LIMIT = 60;
-      private const int MIN_TASK_TIME_LIMIT = 1;
-      private const int MIN_RANDOM_TASKS_COUNT = 2;
-      private const int MAX_TASKS_COUNT = 1000;
+    public partial class Form2 : Form
+    {
+        private const int MAX_TASK_NAME_LENGTH = 10;
+        private const int MAX_TASK_TIME_LIMIT = 60;
+        private const int MIN_TASK_TIME_LIMIT = 1;
+        private const int MIN_RANDOM_TASKS_COUNT = 2;
+        private const int MAX_TASKS_COUNT = 20;
 
-      private Dictionary<string, int> tasks = new Dictionary<string, int>();
-      public Dictionary<string, int> getTasks() => tasks;
-      public Form2()
-      {
-         InitializeComponent();
-         printFirstLine();
-         textBox1.AppendText(Environment.NewLine);
-      }
+        private Dictionary<string, int> tasks = new Dictionary<string, int>();
+        public Dictionary<string, int> getTasks() => tasks;
+        public Form2()
+        {
+            InitializeComponent();
+            printFirstLine();
+            textBox1.AppendText(Environment.NewLine);
+        }
 
-      private void label3_Click(object sender, EventArgs e)
-      {
-
-
-      }
+        private void label3_Click(object sender, EventArgs e) { }
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -39,7 +29,7 @@ namespace dispatcherOSEmu
             this.Close();
         }
 
-        
+
         /**
          * Display generated random tasks
          */
@@ -82,64 +72,64 @@ namespace dispatcherOSEmu
          */
         private bool validateInput()
         {
-         int i = 0;
+            int i = 0;
 
 
-         /**
-         * Validate empty input data
-         */
-         if (String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(textBox3.Text))
-         {
-            MessageBox.Show("enter all fields, data is not valid", "Error", MessageBoxButtons.OK);
-            return false;
-         }
+            /**
+            * Validate empty input data
+            */
+            if (String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("enter all fields, data is not valid", "Error", MessageBoxButtons.OK);
+                return false;
+            }
 
-         /**
-         * Validate incorrect textBox3 integer type
-         */
-         if (!int.TryParse(textBox3.Text, out i))
-         {
-            MessageBox.Show("Incorrect integer value!", "Error", MessageBoxButtons.OK);
-            return false;
-         }
-
-
-         /**
-          *  Validate, if task with the same name already exist
-          */
-         if (getTasks().ContainsKey(textBox2.Text))
-         {
-            MessageBox.Show("Task with the same name already exists", "Error", MessageBoxButtons.OK);
-            return false;
-         }
-
-         /**
-          *  Validate, if textBox1 text length is allowed
-          */
-         if (textBox2.TextLength > MAX_TASK_NAME_LENGTH)
-         {
-            MessageBox.Show("Task name length is too long", "Error", MessageBoxButtons.OK);
-            return false;
-         }
-
-         /**
-          *  Validate, if textBox3 time is greater than maximum allowed 
-          */
-         if (int.Parse(textBox3.Text) > MAX_TASK_TIME_LIMIT)
-         {
-            MessageBox.Show("Task time is too long", "Error", MessageBoxButtons.OK);
-            return false;
-         }
+            /**
+            * Validate incorrect textBox3 integer type
+            */
+            if (!int.TryParse(textBox3.Text, out i))
+            {
+                MessageBox.Show("Incorrect integer value!", "Error", MessageBoxButtons.OK);
+                return false;
+            }
 
 
-         /**
-         *  Validate, if textBox3 time is lower than minimum allowed 
-         */
-         if (int.Parse(textBox3.Text) < MIN_TASK_TIME_LIMIT)
-         {
-            MessageBox.Show("Task time is too small", "Error", MessageBoxButtons.OK);
-            return false;
-         }
+            /**
+             *  Validate, if task with the same name already exist
+             */
+            if (getTasks().ContainsKey(textBox2.Text))
+            {
+                MessageBox.Show("Task with the same name already exists", "Error", MessageBoxButtons.OK);
+                return false;
+            }
+
+            /**
+             *  Validate, if textBox1 text length is allowed
+             */
+            if (textBox2.TextLength > MAX_TASK_NAME_LENGTH)
+            {
+                MessageBox.Show("Task name length is too long", "Error", MessageBoxButtons.OK);
+                return false;
+            }
+
+            /**
+             *  Validate, if textBox3 time is greater than maximum allowed 
+             */
+            if (int.Parse(textBox3.Text) > MAX_TASK_TIME_LIMIT)
+            {
+                MessageBox.Show("Task time is too long", "Error", MessageBoxButtons.OK);
+                return false;
+            }
+
+
+            /**
+            *  Validate, if textBox3 time is lower than minimum allowed 
+            */
+            if (int.Parse(textBox3.Text) < MIN_TASK_TIME_LIMIT)
+            {
+                MessageBox.Show("Task time is too small", "Error", MessageBoxButtons.OK);
+                return false;
+            }
 
             if (tasks.Count > MAX_TASKS_COUNT)
             {
@@ -147,23 +137,22 @@ namespace dispatcherOSEmu
                 return false;
             }
 
-         return true;
+            return true;
 
-      }
+        }
 
 
-      private void printFirstLine()
-      {
-         textBox1.AppendText(String.Format("{0,0} {1, 15}", "Name", "Time Left"));
-         textBox1.AppendText(Environment.NewLine);
-         textBox1.AppendText("----------------------");
-      }
+        private void printFirstLine()
+        {
+            textBox1.AppendText(String.Format("{0,0} {1, 15}", "Name", "Time Left"));
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("----------------------");
+        }
 
-      private void addTaskText()
-      { 
-         textBox1.AppendText(String.Format("{0,-20}{1,-20}", textBox2.Text, textBox3.Text));
-
-      }
+        private void addTaskText()
+        {
+            textBox1.AppendText(String.Format("{0,-20}{1,-20}", textBox2.Text, textBox3.Text));
+        }
 
         private void addTasksText()
         {
@@ -176,19 +165,19 @@ namespace dispatcherOSEmu
         }
 
 
-      private void button1_Click(object sender, EventArgs e)
-      {
-        
-         if(!validateInput())
-         {
-            return;
-         }
+        private void button1_Click(object sender, EventArgs e)
+        {
 
-         tasks.Add(textBox2.Text, int.Parse(textBox3.Text));
-         addTaskText();
-         textBox1.AppendText(Environment.NewLine);
+            if (!validateInput())
+            {
+                return;
+            }
 
-      }
+            tasks.Add(textBox2.Text, int.Parse(textBox3.Text));
+            addTaskText();
+            textBox1.AppendText(Environment.NewLine);
+
+        }
 
         private void Form2_Load(object sender, EventArgs e)
         {
